1d21cc84b67a8b3e00b4b3d4c321c6acf19445ea
cospan <cospan@gmail.com>
2015-12-12 20:00:27 -0500

    Starting to make a level!

--- game/tile_game/tile_game.py
+++ game/tile_game/tile_game.py
@@ -30,6 +30,14 @@ EPILOG = "\n" \
          "\n"
 
 
+def enum(*sequential, **named):
+  enums = dict(zip(sequential, range(len(sequential))), **named)
+  return type('Enum', (), enums)
+
+DIRECTIONS = enum ("UP", "DOWN", "LEFT", "RIGHT")
+STATE = enum ("START", "SETUP_LEVEL", "RUN_LEVEL", "FINISHED")
+
+
 
 FPS             = 30 # frames per second to update the screen
 WINWIDTH        = 800 # width of the program's window, in pixels
@@ -48,20 +56,31 @@ CAM_MOVE_SPEED  = 5 # how many pixels per frame the camera moves
 # decoration on them, such as a tree or rock.
 OUTSIDE_DECORATION_PCT = 20
 
-BRIGHTBLUE      = (  0, 170, 255)
-WHITE           = (255, 255, 255)
-BGCOLOR         = BRIGHTBLUE
-TEXTCOLOR       = WHITE
+GRAY        = (0x7F, 0x7F, 0x7F)
+NAVYBLUE    = (0x3F, 0x3F, 0x7F)
+WHITE       = (0xFF, 0xFF, 0xFF)
+RED         = (0xFF, 0x00, 0x00)
+GREEN       = (0x00, 0xFF, 0x00)
+BLUE        = (0x00, 0x00, 0xFF)
+YELLOW      = (0xFF, 0xFF, 0x00)
+ORANGE      = (0xFF, 0x7F, 0x00)
+PURPLE      = (0xFF, 0x00, 0xFF)
+CYAN        = (0x00, 0xFF, 0xFF)
+BLACK       = (0x00, 0x00, 0x00)
 
-UP              = 'up'
-DOWN            = 'down'
-LEFT            = 'left'
-RIGHT           = 'right'
+BRUSE       = (0x3F, 0x00, 0xFF)
+
+BGCOLOR         = BRUSE
+TEXTCOLOR       = BLACK
+
+
 
 ASSET_BASE      = os.path.join(os.path.dirname(__file__), "assets")
 
 DEFAULT_GAME_NAME = 'Tile Game'
 DEFAULT_LEVEL_FILE = 'levels.txt'
+DEFAULT_BASIC_FONT = 'freesansbold.ttf'
+
 
 class Game(object):
 
@@ -79,6 +98,8 @@ class Game(object):
         pygame.display.set_caption(name)
 
         #Setup the fonts
+        self.basic_font = pygame.font.Font(DEFAULT_BASIC_FONT, 18)
+
         self.images = { 'uncovered goal': pygame.image.load(Game.asset('RedSelector.png')),
                         'covered goal':   pygame.image.load(Game.asset('Selector.png')),
                         'star':           pygame.image.load(Game.asset('Star.png')),
@@ -86,10 +107,10 @@ class Game(object):
                         'wall':           pygame.image.load(Game.asset('Wood_Block_Tall.png')),
                         'inside floor':   pygame.image.load(Game.asset('Plain_Block.png')),
                         'outside floor':  pygame.image.load(Game.asset('Grass_Block.png')),
-                        'title':          pygame.image.load(Game.asset('star_title.png')),
+                        'title':          pygame.image.load(Game.asset('the_path_title.png')),
                         'solved':         pygame.image.load(Game.asset('star_solved.png')),
                         'princess':       pygame.image.load(Game.asset('princess.png')),
-                        'boy':            pygame.image.load(Game.asset('boy.png')),
+                        'jeff':           pygame.image.load(Game.asset('boy.png')),
                         'catgirl':        pygame.image.load(Game.asset('catgirl.png')),
                         'horngirl':       pygame.image.load(Game.asset('horngirl.png')),
                         'pinkgirl':       pygame.image.load(Game.asset('pinkgirl.png')),
@@ -107,8 +128,15 @@ class Game(object):
                             '2': self.images['short tree'],
                             '3': self.images['tall tree'],
                             '4': self.images['ugly tree']}
+
         self.levels = []
+        self.level_map = None
         self.read_levels_file(levels)
+        self.main_character = 'jeff'
+        #Starting Level
+        self.level_num = 0
+        self.current_level = None
+        self.state = STATE.START
 
     def image_map(self, map_item):
         return self.tile_map[map_item]
@@ -121,10 +149,14 @@ class Game(object):
         sys.exit()
 
     def loop(self):
-        for event in pygame.event.get(): # event handling loop
-            if event.type == QUIT:
-                # Player clicked the "X" at the corner of the window.
-                self.terminate()
+        if self.state == STATE.START:
+            self.start_screen()
+
+        elif self.state == STATE.SETUP_LEVEL:
+            self.setup_level()
+
+        elif self.state == STATE.RUN_LEVEL:
+            self.run_level()
 
     def read_levels_file(self, filepath):
         if not os.path.exists(filepath):
@@ -156,27 +188,26 @@ class Game(object):
             elif line == '' and len(map_text_lines) > 0:
                 #Encountered a blank line, this is the end of a level map
                 #Convert the text into an actual level
-  
                 max_width = -1
                 for i in range(len(map_text_lines)):
                     if len(map_text_lines[i]) > max_width:
                         #Adjust width
                         max_width = len(map_text_lines[i])
-  
+
                 #Add spaces to the end of the shorter rows. This ensures the map is
                 #rectangular
                 for i in range(len(map_text_lines)):
                     map_text_lines[i] += ' ' * (max_width - len(map_text_lines[i]))
-  
+
                 #Convert map text lines to a map object
                 for x in range(len(map_text_lines[0])):
                     map_object.append([])
-  
+
                 #Convert list of string to 2D Matrix of objects
                 for y in range(len(map_text_lines)):
                     for x in range(max_width):
                         map_object[x].append(map_text_lines[y][x])
-  
+
                 #Loop through the spaes in a map and find the @, ., and $
                 #Characters for the starting game state
                 startx = None
@@ -193,22 +224,23 @@ class Game(object):
                             goals.append((x, y))
                         if map_object[x][y] in ('$', '*'):
                             stars.append((x, y))
-  
+
                 #Sanity Checks
-                assert startx != None and starty != None, "Level %s (around line %s) in %s is missing a '@' or '+' to make the start point." %(level_num + 1, line_num, filename)
-                assert len(goals) > 0, 'level %s (around line %s) in %s must have at least one goal.' % (level_num + 1, line_num, filename)
-                assert len(stars) <= len(goals), 'level %s (around line %s) in %s is impossible to solve. It has %s goals but ony %s stars.' % (level_num + 1, line_num, filename, len(goals), len(stars))
-  
+                assert startx != None and starty != None, "Level %s (around line %s) in %s is missing a '@' or '+' to make the start point." %(level_num + 1, line_num, filepath)
+                #assert len(goals) > 0, 'level %s (around line %s) in %s must have at least one goal.' % (level_num + 1, line_num, filepath)
+                #assert len(stars) <= len(goals), 'level %s (around line %s) in %s is impossible to solve. It has %s goals but ony %s stars.' % (level_num + 1, line_num, filepath, len(goals), len(stars))
+
                 game_state = {  'player':       (startx, starty),
                                 'step_counter': 0,
                                 'stars':        stars}
-  
+
                 level_object = {'width':        max_width,
                                 'height':       len(map_object),
                                 'map_object':   map_object,
                                 'goals':        goals,
-                                'start_state':  game_state}
-  
+                                'start_state':  game_state,
+                                'state':        None}
+
                 self.levels.append(level_object)
 
                 map_text_lines = []
@@ -216,6 +248,263 @@ class Game(object):
                 game_state = {}
                 level_num += 1
 
+    def start_screen(self):
+        """
+        Display the start screen
+        """
+        title_rect = self.images['title'].get_rect()
+        top_coord = 50
+        title_rect.top = top_coord
+        title_rect.centerx = HALF_WINWIDTH
+        top_coord += title_rect.height
+
+        # Unfortunately, Pygame's font & text system only shows one line at
+        # a time, so we can't use strings with \n newline characters in them.
+        # So we will use a list with each line in it.
+        instruction_text = ['Path... or not, it\'s up to you...',
+                            'Arrow keys to move, WASD for camera control, P to change character.',
+                            'Backspace to reset level, Esc to quit.',
+                            'N for next level, B to go back a level.']
+
+        #Start Drawing a blank color to the entire window
+        self.display_surface.fill(BGCOLOR)
+        self.display_surface.blit(self.images["title"], title_rect)
+
+        #Position and draw the text
+        for i in range(len(instruction_text)):
+            inst_surf = self.basic_font.render(instruction_text[i], 1, TEXTCOLOR)
+            inst_rect = inst_surf.get_rect()
+            top_coord += 10 # 10 Pixels will go in between each line of text
+            inst_rect.top = top_coord
+            inst_rect.centerx = HALF_WINWIDTH
+            top_coord += inst_rect.height
+            self.display_surface.blit(inst_surf, inst_rect)
+
+        while True:
+            #Loop till the user presses something
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.terminate()
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.terminate()
+                    self.state = STATE.SETUP_LEVEL
+                    return
+
+            pygame.display.update()
+            self.fpsclock.tick()
+
+    def run_level(self):
+        key_press = False
+        player_move_to = None
+        state = self.current_level['state']
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT:
+                # Player clicked the "X" at the corner of the window.
+                self.terminate()
+
+            if event.type == KEYDOWN:
+                key_press = True
+                if event.key == K_LEFT:
+                    player_move_to = DIRECTIONS.LEFT
+                if event.key == K_RIGHT:
+                    player_move_to = DIRECTIONS.RIGHT
+                if event.key == K_UP:
+                    player_move_to = DIRECTIONS.UP
+                if event.key == K_DOWN:
+                    player_move_to = DIRECTIONS.DOWN
+
+        redraw_map = False
+        if player_move_to is not None:
+            moved = self.make_move(player_move_to)
+
+            if moved:
+                state['step_counter'] += 1
+                redraw_map = True
+
+        if redraw_map:
+               self.draw_map() 
+
+        self.display_surface.fill(BGCOLOR)
+        map_surface_rect = self.level_map.get_rect()
+        map_surface_rect.center = (HALF_WINWIDTH, HALF_WINHEIGHT)
+
+        self.display_surface.blit(self.level_map, map_surface_rect)
+
+        pygame.display.update()
+        self.fpsclock.tick()
+
+    def setup_level(self):
+        self.current_level = copy.deepcopy(self.levels[self.level_num])
+        start_state = copy.deepcopy(self.current_level["start_state"])
+        level_surface = self.basic_font.render("Level %s of %s" % (self.level_num + 1, len(self.levels)), 1, TEXTCOLOR)
+        level_rect = level_surface.get_rect()
+        level_rect.bottomleft = (20, WINHEIGHT - 35)
+        map_object = self.decorate_map()
+        self.current_level["map_object"] = map_object
+        state = {   "x":            start_state["player"][0],
+                    "y":            start_state["player"][1],
+                    "step_counter": 0,
+                    "stars":        start_state["stars"]}
+        self.current_level["state"] = state
+        self.draw_map()
+        self.state = STATE.RUN_LEVEL
+
+    def draw_map(self):
+        map_object = self.current_level["map_object"]
+        state = self.current_level["state"]
+        goals = self.current_level["goals"]
+        map_width = len(map_object) * TILEWIDTH
+        map_height = (len(map_object[0]) - 1) * TILEFLOORHEIGHT + TILEHEIGHT
+        map_surface = pygame.Surface((map_width, map_height))
+        map_surface.fill(BGCOLOR)
+
+        for x in range(len(map_object)):
+            for y in range(len(map_object[x])):
+                space_rect = pygame.Rect((x * TILEWIDTH, y * TILEFLOORHEIGHT, TILEWIDTH, TILEHEIGHT))
+                if map_object[x][y] in self.tile_image:
+                    base_tile = self.tile_image[map_object[x][y]]
+                elif map_object[x][y] in self.out_image:
+                    base_tile = self.tile_image[' ']
+
+                map_surface.blit(base_tile, space_rect)
+                if map_object[x][y] in self.out_image:
+                    map_surface.blit(self.out_image[map_object[x][y]], space_rect)
+
+                elif (x, y) in state['stars']:
+                    if (x, y) in goals:
+                        # A goal AND star are on thsi space, draw goal first
+                        map_surface.blit(self.images["covered goal"], space_rect)
+
+                    #Draw the star afterwards (It will be on top!)
+                    map_surface.blit(self.images["star"], space_rect)
+
+                elif (x, y) in goals:
+                    #Draw a goal without a star on it
+                    map_surface.blit(self.images['uncovered goal'], space_rect)
+
+                if (x, y) == (state['x'], state['y']):
+                    map_surface.blit(self.images[self.main_character], space_rect)
+
+        self.level_map = map_surface
+
+    def decorate_map(self):
+        '''
+        Replace the nothingness void that is my life outside the walls of the
+        level with random prettyness
+        '''
+        startx = self.current_level['start_state']['player'][0]
+        starty = self.current_level['start_state']['player'][1]
+        map_object = self.current_level['map_object']
+        for x in range(len(map_object)):
+            for y in range(len(map_object[0])):
+                if map_object[x][y] in ('$', '.', '@', '+', '*'):
+                    map_object[x][y] = ' '
+        #Flood fill to determine inside and outside of floor tiles
+        self.flood_fill(map_object, startx, starty, ' ', 'o')
+
+        #Convert adjoined walls into corner tiles
+        for x in range(len(map_object)):
+            for y in range(len(map_object[0])):
+                if map_object[x][y] == '#':
+                    if (self.is_wall(x, y-1) and self.is_wall(x+1, y)) or \
+                       (self.is_wall(x+1, y) and self.is_wall(x, y+1)) or \
+                       (self.is_wall(x, y+1) and self.is_wall(x-1, y)) or \
+                       (self.is_wall(x-1, y) and self.is_wall(x, y-1)):
+                        map_object[x][y] = 'x'
+
+                elif map_object[x][y] == ' ' and random.randint(0, 99) < OUTSIDE_DECORATION_PCT:
+                    map_object[x][y] = random.choice(list(self.out_image.keys()))
+        return map_object
+
+
+
+    def is_blocked(self, x, y):
+        """Returns True if the (x, y) position on the map is
+        blocked by a wall or star, otherwise return False."""
+        map_object = self.current_level["map_object"]
+        state = self.current_level['state']
+
+        if self.is_wall(x, y):
+            return True
+
+        elif x < 0 or x >= len(map_object) or y < 0 or y >= len(map_object[x]):
+            return True # x and y aren't actually on the map.
+
+        elif (x, y) in state['stars']:
+            return True # a star is blocking
+
+        return False
+
+
+
+    def is_wall(self, x, y):
+        """Returns True if the (x, y) position on
+        the map is a wall, otherwise return False."""
+        map_object = self.current_level['map_object']
+        if x < 0 or x >= len(map_object) or y < 0 or y >= len(map_object[x]):
+            return False # x and y aren't actually on the map.
+        elif map_object[x][y] in ('#', 'x'):
+            return True # wall is blocking
+        return False
+
+    def flood_fill(self, map_object, x, y, old_char, new_char):
+        """Changes any values matching oldCharacter on the map object to
+        newCharacter at the (x, y) position, and does the same for the
+        positions to the left, right, down, and up of (x, y), recursively."""
+
+        # In this game, the flood fill algorithm creates the inside/outside
+        # floor distinction. This is a "recursive" function.
+        # For more info on the Flood Fill algorithm, see:
+        #   http://en.wikipedia.org/wiki/Flood_fill
+
+        if map_object[x][y] == old_char:
+            map_object[x][y] = new_char
+
+        if x < len(map_object) - 1 and map_object[x+1][y] == old_char:
+            self.flood_fill(map_object, x+1, y, old_char, new_char) # call right
+        if x > 0 and map_object[x-1][y] == old_char:
+            self.flood_fill(map_object, x-1, y, old_char, new_char) # call left
+        if y < len(map_object[x]) - 1 and map_object[x][y+1] == old_char:
+            self.flood_fill(map_object, x, y+1, old_char, new_char) # call down
+        if y > 0 and map_object[x][y-1] == old_char:
+            self.flood_fill(map_object, x, y-1, old_char, new_char) # call up
+
+    #Movement Functions
+    def make_move(self, move_to):
+        state = self.current_level["state"]
+        px = state['x']
+        py = state['y']
+        stars = state['stars']
+        map_object = self.current_level["map_object"]
+
+        if move_to == DIRECTIONS.UP:
+            x_offset = 0
+            y_offset = -1
+        elif move_to == DIRECTIONS.RIGHT:
+            x_offset = 1
+            y_offset = 0
+        elif move_to == DIRECTIONS.DOWN:
+            x_offset = 0
+            y_offset = 1
+        elif move_to == DIRECTIONS.LEFT:
+            x_offset = -1
+            y_offset = 0
+
+        if self.is_wall(px + x_offset, py + y_offset):
+            return False
+
+        if (px + x_offset, py + y_offset) in stars:
+            if not self.is_blocked(px + (x_offset * 2), py + (y_offset * 2)):
+                #Move the star
+                ind = stars.index((px + x_offset, py + y_offset))
+                stars[ind] = (stars[ind][0] + x_offset, stars[ind][1] + y_offset)
+            else:
+                return False
+
+        state['x'] = px + x_offset
+        state['y'] = py + y_offset
+        return True
 
 
 def main(argv):
