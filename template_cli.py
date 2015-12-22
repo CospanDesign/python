264b564286bf64d1756f5ce9753535d72e814ee2
cospan <cospan@gmail.com>
2015-12-22 15:37:41 -0500

    Modified the license at the top of template_cli.py

--- template_cli.py
+++ template_cli.py
@@ -2,18 +2,18 @@
 
 # Copyright (c) 2015 Dave McCoy (dave.mccoy@cospandesign.com)
 #
-# Nysa is free software; you can redistribute it and/or modify
+# NAME is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # any later version.
 #
-# Nysa is distributed in the hope that it will be useful,
+# NAME is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Nysa; If not, see <http://www.gnu.org/licenses/>.
+# along with NAME; If not, see <http://www.gnu.org/licenses/>.
 
 
 import sys
