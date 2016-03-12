b4a2392b526eaad3124d5078208a82a01f4698f2
cospan <cospan@gmail.com>
2016-03-12 12:19:55 -0500

    Updated template CLI

--- template_cli.py
+++ template_cli.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Copyright (c) 2015 Dave McCoy (dave.mccoy@cospandesign.com)
+# Copyright (c) 2016 Dave McCoy (dave.mccoy@cospandesign.com)
 #
 # NAME is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
